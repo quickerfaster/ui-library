83493e9c784382e4ab6d97b83c11b92c6702f41f
Aminu A Mohammed <honestee2013@gmail.com>
2026-08-18 05:07:25 +0100

    fix(organization): replace hr_admin role with organization_manager

--- src/Core/Admin/Data/dashboard.php
+++ src/Core/Admin/Data/dashboard.php
@@ -293,7 +293,7 @@ return array (
   array (
     'admin' => 'full',
     'super_admin' => 'full',
-    'hr_admin' => 'limited',
+    'organization_manager' => 'limited',
   ),
   'layout' =>
   array (
